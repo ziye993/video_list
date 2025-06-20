63517bc5f7aaddc6e097ef5969cc4821837ced2b
ziye993 <2996447644@qq.com>
2025-06-20 19:13:37 +0800

    配置默认路径

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +10,5 @@ export default defineConfig({
     port: 5173, // 自定义端口
     open: true, // 自动打开浏览器
   },
+  base: '/videolist/',
 })
